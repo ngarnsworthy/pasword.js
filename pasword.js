79bc33aa8aec0cf7448dc3c8561dd497501f6ab0
ngarnsworthy <ngarnsworthy@gmail.com>
2018-12-24 09:36:35 -0600

    update acount

--- pasword.js
+++ pasword.js
@@ -3,15 +3,38 @@ var passwords = [];
 
 fs.readFile( 'db.txt', (err, data) => {
   passwords = JSON.stringify(data);
-})
+});
+
+/**
+ *
+ */
+function find(a_username, a_password) {
+  const val = passwords.find( function(obj) {
+    if( obj.un === a_name ) {
+      return true;
+    }
+  });
+  if(val) {
+    if( val.pw === a_password ) {
+      return  val;
+    }
+  }
+}
+
+/**
+ *
+ */
+ var writeDB = function() {
+   fs.writeFile( 'db.txt', JSON.stringify(passwords), err => {
+     if(err) throw err;
+   }
+ };
 
 return {
   /** Add a user to the system */
   add: function( a_username, a_password, a_accountVal = 0 ) {
     passwords.push( { un: a_username, pw: a_password, av: a_accountVal } );
-    fs.writeFile( 'db.txt', JSON.stringify(passwords), err => {
-      if(err) throw err;
-    }
+    writeDB();
   },
 
   /** Check the balance for a user in the system
@@ -21,20 +44,15 @@ return {
     * @returns {number|undefined} account value or undefined
     */
   check: function( a_name, a_password ) {
-    let ret = undefined;
+    let ret = find( a_name, a_password)
+    return ret && ret.av;
+  },
 
-    const val = passwords.find( function(obj) {
-      if( obj.un === a_name ) {
-        return true;
-      }
-    });
-
-    if(val) {
-      if( val.pw === a_password ) {
-        ret = val.av;
-      }
-    }
-
-    return ret;
+  account: function ( a_username, a_password, a_accountValIncrese) {
+    let acount = find(a_username, a_password);
+if (acount) {
+  acount.av+=a_accountValIncrese
+  writeDB()
+}
   }
 };
